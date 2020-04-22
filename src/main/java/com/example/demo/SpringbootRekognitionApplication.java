030b48b5fc9d87d7ec90bd2a7e049a186d357143
c5apple <c5apple_party@yahoo.co.jp>
2020-04-22 15:35:07 +0900

    バッチ実行準備

--- src/main/java/com/example/demo/SpringbootRekognitionApplication.java
+++ src/main/java/com/example/demo/SpringbootRekognitionApplication.java
@@ -2,12 +2,18 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringbootRekognitionApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootRekognitionApplication.class, args);
-	}
+		//		SpringApplication.run(SpringbootRekognitionApplication.class, args);
 
+		try (ConfigurableApplicationContext ctx = SpringApplication.run(SpringbootRekognitionApplication.class, args)) {
+			SpringbootRekognitionApplication app = ctx.getBean(SpringbootRekognitionApplication.class);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
