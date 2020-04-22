be63387f9d0b3f7583db0d2465b4e5a54d1a2cb7
c5apple <c5apple_party@yahoo.co.jp>
2020-04-22 16:04:31 +0900

    イメージ内の顔の検出

--- src/main/java/com/example/demo/SpringbootRekognitionApplication.java
+++ src/main/java/com/example/demo/SpringbootRekognitionApplication.java
@@ -14,16 +14,22 @@ import org.springframework.context.ConfigurableApplicationContext;
 
 import com.amazonaws.services.rekognition.AmazonRekognition;
 import com.amazonaws.services.rekognition.AmazonRekognitionClientBuilder;
+import com.amazonaws.services.rekognition.model.AgeRange;
 import com.amazonaws.services.rekognition.model.AmazonRekognitionException;
+import com.amazonaws.services.rekognition.model.Attribute;
+import com.amazonaws.services.rekognition.model.DetectFacesRequest;
+import com.amazonaws.services.rekognition.model.DetectFacesResult;
 import com.amazonaws.services.rekognition.model.DetectLabelsRequest;
 import com.amazonaws.services.rekognition.model.DetectLabelsResult;
 import com.amazonaws.services.rekognition.model.DetectModerationLabelsRequest;
 import com.amazonaws.services.rekognition.model.DetectModerationLabelsResult;
+import com.amazonaws.services.rekognition.model.FaceDetail;
 import com.amazonaws.services.rekognition.model.Image;
 import com.amazonaws.services.rekognition.model.Label;
 import com.amazonaws.services.rekognition.model.ModerationLabel;
 import com.amazonaws.services.rekognition.model.S3Object;
 import com.amazonaws.util.IOUtils;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootApplication
 public class SpringbootRekognitionApplication {
@@ -35,6 +41,7 @@ public class SpringbootRekognitionApplication {
 			SpringbootRekognitionApplication app = ctx.getBean(SpringbootRekognitionApplication.class);
 			app.getDetectLabels();
 			app.getDetectLabelsForLocal();
+			app.getDetectFaces();
 			app.getDetectModerationLabels();
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -100,6 +107,51 @@ public class SpringbootRekognitionApplication {
 			List<Label> labels = result.getLabels();
 			for (Label label : labels) {
 				System.out.println(label.getName() + ": " + label.getConfidence().toString());
+			}
+		} catch (AmazonRekognitionException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * イメージ内の顔の検出
+	 */
+	private void getDetectFaces() throws FileNotFoundException, IOException {
+		// ローカルファイルの画像
+		String filePath = "C:\\photo.jpg";
+		ByteBuffer imageBytes;
+		try (InputStream inputStream = new FileInputStream(new File(filePath))) {
+			imageBytes = ByteBuffer.wrap(IOUtils.toByteArray(inputStream));
+		}
+		Image imageObject = new Image().withBytes(imageBytes);
+
+		AmazonRekognition rekognitionClient = AmazonRekognitionClientBuilder.defaultClient();
+
+		DetectFacesRequest request = new DetectFacesRequest()
+				.withImage(imageObject)
+				.withAttributes(Attribute.ALL);
+		// Replace Attribute.ALL with Attribute.DEFAULT to get default values.
+
+		try {
+			// 画像分析を実行する
+			DetectFacesResult result = rekognitionClient.detectFaces(request);
+
+			// 結果を出力する
+			System.out.println("Detected labels for " + filePath);
+			List<FaceDetail> faceDetails = result.getFaceDetails();
+			for (FaceDetail face : faceDetails) {
+				if (request.getAttributes().contains("ALL")) {
+					AgeRange ageRange = face.getAgeRange();
+					System.out.println("The detected face is estimated to be between "
+							+ ageRange.getLow().toString() + " and " + ageRange.getHigh().toString()
+							+ " years old.");
+					System.out.println("Here's the complete set of attributes:");
+				} else { // non-default attributes have null values.
+					System.out.println("Here's the default set of attributes:");
+				}
+
+				ObjectMapper objectMapper = new ObjectMapper();
+				System.out.println(objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(face));
 			}
 		} catch (AmazonRekognitionException e) {
 			e.printStackTrace();
